9d75fe548276cce2e1ab865e0fe56ae9cc522072
GovindLohar9109 <govind@gkmit.co>
2025-10-27 22:10:01 +0530

    Updated

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
 import express from 'express';
-
+import dotenv from "dotenv";
+const PORT=process.env.PORT || 8000;
 const app = express();
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from CI/CD test app!' });
 });
 
+
+app.listen(PORT,()=>{
+  console.log("Server is running on PORT ",PORT);
+})
+
 export default app;
 
-// Start server only when not in test mode
-if (process.env.NODE_ENV !== 'test') {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-}
